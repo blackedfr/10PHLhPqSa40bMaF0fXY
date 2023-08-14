b68a4995570007913df53f25d70eefa256fe2178
TWK6OR2LREDOCN4T5B5F <135587611+TWK6OR2LREDOCN4T5B5F@users.noreply.github.com>
2023-08-14 10:49:02 -0500

    Update pLHynIAncpHDyfpUDSFK.lua
    testing 🥉

--- pLHynIAncpHDyfpUDSFK.lua
+++ pLHynIAncpHDyfpUDSFK.lua
@@ -1,5 +1,5 @@
 
-local Testing = false
+local Testing = true
 -- Cmds
 if table.find(getgenv().Alts,game.Players.LocalPlayer.UserId) then
 	getgenv().PointInTable = table.find(getgenv().Alts,game.Players.LocalPlayer.UserId)
@@ -223,7 +223,7 @@ local SetupsTable = {
 	},
 	Admin = {
 		Origin = CFrame.new(-884.12915, -38.3972931, -545.291809, -0.99998939, 2.69316498e-08, -0.00460755778, 2.6944301e-08, 1, -2.68358624e-09, 0.00460755778, -2.80770518e-09, -0.99998939),
-		ZMultiplier = 8,
+		ZMultiplier = 3,
 		XMultiplier = 8,
 		PerRow = 10,
 		Rows = 4,
